094eb22451c84a5c64d2fb214bd6757ad52e4fab
Tahsin Sayeed <tahsinalsayeed@gmail.com>
2023-03-15 19:00:20 +0600

    Deploy EC2

--- starter-project/main.tf
+++ starter-project/main.tf
@@ -10,4 +10,11 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
+  profile = "experiment"
+}
+
+
+resource "aws_instance" "web" {
+    ami = "ami-0f8ca728008ff5af4"
+    instance_type = "t3.micro"
 }
