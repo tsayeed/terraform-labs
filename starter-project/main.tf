80830ee572742bbda293687575d793388517ac09
Tahsin Sayeed <tahsinalsayeed@gmail.com>
2023-03-15 19:12:48 +0600

    Destroy deployed EC2

--- starter-project/main.tf
+++ starter-project/main.tf
@@ -14,11 +14,11 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "web" {
-    ami = "ami-0f8ca728008ff5af4"
-    instance_type = "t3.micro"
+# resource "aws_instance" "web" {
+#     ami = "ami-0f8ca728008ff5af4"
+#     instance_type = "t3.micro"
 
-    tags = {
-      "Name" = "Ubuntu Server"
-    }
-}
+#     tags = {
+#       "Name" = "Ubuntu Server"
+#     }
+# }
