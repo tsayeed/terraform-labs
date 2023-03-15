37a281aca846acca89868702992ff9c687136977
Tahsin Sayeed <tahsinalsayeed@gmail.com>
2023-03-15 19:28:16 +0600

    Reference other resource attributes(VPC & subnet)

--- starter-project/main.tf
+++ starter-project/main.tf
@@ -13,6 +13,22 @@ provider "aws" {
   profile = "experiment"
 }
 
+resource "aws_vpc" "production" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id     = aws_vpc.production.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Prod Subnet"
+  }
+}
 
 # resource "aws_instance" "web" {
 #     ami = "ami-0f8ca728008ff5af4"
